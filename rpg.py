790861be4b2781a069ab9ba797329890240ec013
Matt Langhinrichs <mlanghinrichs@gmail.com>
2018-11-09 02:45:28 -0500

    v1 of name generator in rpg.py

--- rpg.py
+++ rpg.py
@@ -20,4 +20,28 @@ def c_tbl(filename):
     system resources."""
     return choice(tbl(filename))
 
-# print(c_tbl("README"))
+
+def ngen_l(list_):
+    """n(ame)gen_l(ist) - Generate names from strings in list."""
+    """Input format: 'x ' for choice of x or nul; 'x y' for choice of x or y; 'x y '
+    for a choice of x or y or nul. Pass a list in the order of generation, which will be
+    concatenated."""
+    out = ""
+    for string in list_:
+        out += choice(string.split(" "))
+    if len(out) > 1:
+        return out[0].upper() + out[1:].lower()
+    elif len(out) == 1:
+        return out[0].upper()
+    else:
+        return "No name..."
+
+def ngen_f(filename):
+    """n(ame)gen_f(ile) - Pass a file, return a string with generated name."""
+    namelist = tbl(filename)
+    if namelist[0] == "LIST":
+        return choice(namelist)
+    else:
+        ngen_l(namelist)
+
+print(ngen_f("tbls/names/fantasy_dwarf_male.txt"))
