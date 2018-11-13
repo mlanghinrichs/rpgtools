9be5396db4d436ca92825c1351f39be8b2d3598a
Matt Langhinrichs <mlanghinrichs@gmail.com>
2018-11-13 01:51:04 -0500

    Merged goh.py into rpg.py, generalized via json.

--- rpg.py
+++ rpg.py
@@ -1,35 +1,22 @@
 from random import choice
-import json
+import json, os
 
-# Import json name data into ndata dict before doing anything else
+# Import json name, adventure data into dicts before doing anything else
 ndata = {}
 with open("tbls/names.json", "r") as f:
     ndata = json.load(f)
 
-
-def tbl(filename):
-    """Return list with \\n stripped from lines in filename, making a 'table' to roll."""
-
-    output = []
-
-    try:
-        with open(filename, "r") as f:
-            for line in f.readlines():
-                line = line[:-1] # Strip last char (probably newline)
-                if len(line): # Check to avoid adding empty lines from file
-                    output.append(line) # Append the line to output list
-    except FileNotFoundError:
-        output.append("Missing file!")
-
-    return output
+advdata = {}
+with open("tbls/adv.json", "r") as f:
+    advdata = json.load(f)
 
 
-def tbl_json(arg_tuple):
+def tbl_json(dic, arg_tuple):
     """Takes a set of keys in a tuple and returns (list, listtype) tuple from ndata."""
     """e.g., ('foo', 'bar') as argument would return (ndata['foo']['bar'], 'list') if it
     found an array and (ndata['foo']['bar'], 'generator') if it found a string. Passed as
     tuple and not raw args to make passing the input through multiple funcs easier."""
-    current = ndata # working dict starts as ndata
+    current = dic # working dict starts as dic
     
     try:
         for arg in arg_tuple: # for each path in the tuple,
@@ -42,8 +29,13 @@ def tbl_json(arg_tuple):
             else: # if it finds none of these, throw an error
                 raise IndexError('tbl_json called on sequence not ending in a string.')
     except KeyError:
-        print("Tried to pull a nonexistent generator with tbl_json! Returning ''")
+        print(f"Tried to pull a nonexistent generator {arg_tuple} with tbl_json! Returning ''")
         return ([''], 'list')
+
+
+def jchoice(dic, tup):
+    return choice(tbl_json(dic,tup)[0])
+
 
 def ngen_l(list_):
     """n(ame)gen_l(ist) - Return a name string generated from inputted generator list."""
@@ -65,7 +57,7 @@ def ngen_l(list_):
 
 def ngen_j(arg_tuple):
     """Takes a set of keys in a tuple and returns a generated name from ndata."""
-    inp = tbl_json(arg_tuple)
+    inp = tbl_json(ndata, arg_tuple)
 
     # Proper list vs generator behavior - generate from generator, pick from list
     if inp[1] == "generator":
@@ -74,54 +66,6 @@ def ngen_j(arg_tuple):
         return choice(inp[0])
     else:
         raise TypeError('Bad/lacking tuple passes to ngen_j!')
-
-
-def ngen_f(filename, number=0):
-    """n(ame)gen_f(ile) - Returns a string with a generated name or a list containing # names."""
-    
-    # Pull file contents into list using tbl(), return "" and print warning if empty
-    namelist = tbl(filename)
-    if len(namelist) == 0:
-        return ""
-        print("ngen_f() pulled '' from empty file")
-    
-    if not number: # If number was left as 0, return a single choice
-        # LIST-headed files are not generators, but just a list of names...
-        if namelist[0] == "LIST":
-            # ...so pop off that LIST and choice() accordingly
-            namelist.remove("LIST")
-            return choice(namelist)
-        # Otherwise, ngen_l() the list made from the file
-        else:
-            return ngen_l(namelist)
-    elif number > 0: # If num > 0, drop num of choices into the output list and return it
-        output = []
-        if namelist[0] == "LIST": # As above
-            namelist.remove("LIST")
-            for i in range(number):
-                output.append(choice(namelist))
-            return output
-        else: # As above
-            for i in range(number):
-                output.append(ngen_l(namelist))
-            return output
-
-
-def ngen_fkeys(path, *args):
-    """ngen_f(), but passed path .../ and terms from a list instead of an explicit filename."""
-    """E.g., 'fantasy', 'dwarf', 'male' for fantasy_dwarf_male.txt. path MUST end in /"""
-    
-    fullpath = path
-    
-    for arg in args: # For each non-positional argument
-        fullpath += arg # Append it to the file path
-        if not args[-1] == arg: # And unless it's the last argument
-            fullpath += "_" # Append an underscore
-        else: # But if it is
-            fullpath += ".txt" # Just append the extension
-
-    # And run ngen_f() on that full path .../termx_termy_termz.txt
-    return ngen_f(fullpath)
 
 
 class Character:
@@ -143,7 +87,67 @@ class Character:
         else:
             self.name = ngen_j((namesource, race, gender))
             self.surname = ngen_j((namesource, race, "surnames"))
-            
+
+
+class Adventure:
+    """A class of Adventure objects, containing data/functions for a session."""
+
+    def __init__(self, advtype, *, num_elements=3, **kwargs):
+        # Temp variables to increase legibility
+        d = advdata
+        t = advtype
+        
+        # Set all adventure variables
+        self.locale = jchoice(d, (t, 'locales'))
+        self.sub_locale = jchoice(d, (t, 'sub_locales'))
+        self.plot = jchoice(d, (t, 'plots')) 
+        self.objective = jchoice(d, (t, 'objectives'))
+        self.hours = [jchoice(d, (t, 'hours')) for x in range(3)]
+        self.story_elements = [jchoice(d, (t, 'story_elements')) for x in range(num_elements)]
+
+        # If user manually entered any details, overwrite the generated ones
+        self.__dict__.update((k, v) for k,v in vars().items() if k in ['locale',
+            'sub_locale', 'plot', 'objective', 'hours', 'story_elements', 'num_elements'])
+
+        # Finally, set a title for the adventure
+        self.title = "THE " + self.story_elements[0].upper() + " OF " + self.locale.upper()
+        
+    
+    def desc(self):
+        """Return a formatted str containing the adventure details written out."""
+
+        out = f"""{self.title}
+In {self.locale}, in {self.sub_locale};
+A {self.plot}, to {self.objective}.
+
+In hour 1, {self.hours[0]}.
+In hour 2, {self.hours[1]}.
+In hour 3, {self.hours[2]}.
+   
+Random elements:"""
+
+        # Generalizable to any # of elements
+        for n in range(len(self.story_elements)):
+            out += "\n{}. {}".format(n+1, self.story_elements[n])
+
+        # Return the description string
+        return out
+
+    def write(self, direc):
+        """Write adventure details to a txt file in ./direc/ named after the title."""
+        
+        # Str containing path to save file to
+        path = direc + "/" + self.title.replace(" ", "") + ".txt"
+        path = path.lower() # fix dramatic title capitalization
+        if not os.path.isfile(path): # make sure the filename doesn't exist yet
+            with open(path, "w") as f:
+                f.write(self.desc())
+            print("Wrote to " + path)
+
+
+x = Adventure("goh")
+print("\n", x.desc(), "\n")
+
 
 # Main input loop - asks what you want to generate, then (currently) prints name(s) and surname(s)
 while True:
