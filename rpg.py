88d0428d2d8e1c04c8e9ac4c1799cbec18cc207c
Matt Langhinrichs <mlanghinrichs@gmail.com>
2018-11-16 03:53:57 -0500

    Quirk and motivation implementation in Character

--- rpg.py
+++ rpg.py
@@ -1,42 +1,36 @@
-from random import choice, randint
+from random import choice, randint, sample
 import json, os
 
 # Import json name, adventure data into dicts before doing anything else
-ndata = {}
-with open("tbls/names.json", "r") as f:
-    ndata = json.load(f)
+def load_dict(filename):
+    with open("tbls/" + filename, "r") as f:
+        return json.load(f)
 
-adv_data = {}
-with open("tbls/adv.json", "r") as f:
-    adv_data = json.load(f)
+char_dict = load_dict("char.json")
+adv_dict = load_dict("adv.json")
 
+def extract(dict_, *args):
+    """Return dict_[arg1][arg2]... if the value is a string or list."""
 
-def tbl_json(dict_, *args):
-    # TODO make me make more sense & pass a list instead of a tuple by default
-    """Takes a set of keys in a tuple and returns (list, listtype) tuple from ndata."""
-    """e.g., ('foo', 'bar') as argument would return (ndata['foo']['bar'], 'list') if it
-    found an array and (ndata['foo']['bar'], 'generator') if it found a string. Passed as
-    tuple and not raw args to make passing the input through multiple funcs easier."""
-    current = dict_ # working dict starts as dict_
+    current = dict_ ## working dict starts as dict_
     
     try:
-        for arg in args: # for each path in the tuple,
-            if isinstance(current[arg], dict): # if it finds a dict, go a level deeper
-                current = current[arg]
-            elif isinstance(current[arg], str): # if it finds a string, return the split list and a generator marker
-                return (current[arg].split(";"), "generator")
-            elif isinstance(current[arg], list): # if it finds a list, return the list and a list marker
-                return (current[arg], "list")
-            else: # if it finds none of these, throw an error
-                raise IndexError('tbl_json called on sequence not ending in a string.')
+        for arg in args[:-1]:
+            current = current[arg]
+        return current[args[-1]]
     except KeyError:
-        print(f"Tried to pull a nonexistent generator {args} with tbl_json! Returning ''")
-        return ([''], 'list')
+        # Return a list with blank str so name generation uses choice() and not ngen_l()
+        print(f"Tried to pull a nonexistent generator {args} with extract! Returning ['']")
+        return ([''])
 
 
-def jchoice(dict_, *args):
-    # TODO make me less dumb (by making tbl_json less dumb)
-    return choice(tbl_json(dict_, *args)[0])
+def extract_choice(dct, *args):
+    lst = extract(dct, *args)
+    try:
+        return choice(lst)
+    except TypeError:
+        print(f"extract_choice tried to choose from a {type(lst)}")
+        return ''
 
 
 def ngen_l(list_):
@@ -47,27 +41,27 @@ def ngen_l(list_):
     
     out = ""
 
-    for string in list_: # For each line of the generator file,
-        out += choice(string.split(",")) # Append one of the options on that line to out
+    for string in list_: ## For each line of the generator file,
+        out += choice(string.split(",")) ## Append one of the options on that line to out
     
     # TODO - make this if/else to avoid sneaky bugs?
-    try: # Try to capitalize out properly; if it's < 2 chars, skip this to avoid error 
+    try: ## Try to capitalize out properly; if it's < 2 chars, skip this to avoid error 
         return out[0].upper() + out[1:].lower()
     except IndexError:
         return out.upper()
 
 
 def ngen_j(*args):
-    """Takes a set of keys in a tuple and returns a generated name from ndata."""
-    inp = tbl_json(ndata, *args)
+    """Return a generated name from char_dict using the args as a path to the generator."""
+    inp = extract(char_dict, *args)
 
     # Proper list vs generator behavior - generate from generator, pick from list
-    if inp[1] == "generator":
-        return ngen_l(inp[0])
-    elif inp[1] == "list":
-        return choice(inp[0])
+    if isinstance(inp, str):
+        return ngen_l(inp.split(';'))
+    elif isinstance(inp, list):
+        return choice(inp)
     else:
-        raise TypeError('Bad/lacking tuple passed to ngen_j!')
+        raise TypeError(f'{type(inp)} is neither a string nor a list!')
 
 
 class Character:
@@ -81,34 +75,48 @@ class Character:
         self.__dict__.update((k, v) for k,v in vars().items() if k != 'self')
 
         # Generate name and surname from value provided
-        # self.name = ngen_fkeys("tbls/names/", setting, race, gender)
-        # self.surname = ngen_fkeys("tbls/names/", setting, race, "surnames")
+        ## self.name = ngen_fkeys("tbls/names/", setting, race, gender)
+        ## self.surname = ngen_fkeys("tbls/names/", setting, race, "surnames")
         self.name = ngen_j(setting, race, gender)
-        self.surname = ngen_j(setting, race, "surnames")
+        self.surname = ngen_j(setting, race, 'surnames')
 
-        self.age = randint(18,80)
+        # Get agerange list (should be two items) from char_dict
+        self.age = randint(*extract(char_dict, setting, race, 'agerange'))
+
+        # Get features
+        for feature in ('quirk', 'strength', 'flaw', 'desire', 'fear'):
+            self.__dict__[feature] = extract_choice(char_dict, setting, feature)
 
     def desc(self):
-        out = "Name: " + self.name + " " + self.surname
-        out += "\nAge: " + str(self.age)
-        return out
+        rstr = "Name: " + self.name + " " + self.surname
+        rstr += "\nAge: " + str(self.age)
+        for feature in ('quirk', 'strength', 'flaw', 'desire', 'fear'):
+            title = feature[0].upper() + feature[1:]
+            rstr += f"\n{title}: {self.__dict__[feature]}"
+        out = []
+        for line in rstr.split("\n"):
+            h1 = line.split()[0]
+            h2 = " ".join(line.split()[1:])
+            h1 += (12 - len(h1)) * "-"
+            out.append(h1 + h2)
+        return "\n".join(out)
 
 
 class Adventure:
     """A class of Adventure objects, containing data/functions for a session."""
 
-    def __init__(self, advtype, *, num_elements=3, **kwargs):
+    def __init__(self, adv_type, *, num_hours=3, num_elements=3, **kwargs):
         # Temp variables to increase legibility
-        d = adv_data
-        t = advtype
+        d = adv_dict
+        t = adv_type
         
         # Set all adventure variables
-        self.locale = jchoice(d, t, 'locales')
-        self.sub_locale = jchoice(d, t, 'sub_locales')
-        self.plot = jchoice(d, t, 'plots') 
-        self.objective = jchoice(d, t, 'objectives')
-        self.hours = [jchoice(d, t, 'hours') for x in range(3)]
-        self.story_elements = [jchoice(d, t, 'story_elements') for x in range(num_elements)]
+        self.locale = extract_choice(d, t, 'locales')
+        self.sub_locale = extract_choice(d, t, 'sub_locales')
+        self.plot = extract_choice(d, t, 'plots') 
+        self.objective = extract_choice(d, t, 'objectives')
+        self.hours = sample(extract(d, t, 'hours'), num_hours)
+        self.story_elements = sample(extract(d, t, 'story_elements'), num_elements)
 
         # If user manually entered any details, overwrite the generated ones
         self.__dict__.update((k, v) for k,v in vars().items() if k in ['locale',
@@ -143,8 +151,8 @@ Random elements:"""
         
         # Str containing path to save file to
         path = direc + "/" + self.title.replace(" ", "") + ".txt"
-        path = path.lower() # fix dramatic title capitalization
-        if not os.path.isfile(path): # make sure the filename doesn't exist yet
+        path = path.lower() ## fix dramatic title capitalization
+        if not os.path.isfile(path): ## make sure the filename doesn't exist yet
             with open(path, "w") as f:
                 f.write(self.desc())
             print("Wrote to " + path)
@@ -162,48 +170,48 @@ def mainLoop():
             print("- adv(enture): opens adventure generator")
             print("- q(uit): quits program")
         elif inp[0] == "char" or inp[0] == "character":
-            char = Character(*inp[1:]) # Pass args after 'char'/'character' to Character
+            char = Character(*inp[1:]) ## Pass args after 'char'/'character' to Character
             print(char.desc())
             # if charLoop():
             #     break
         elif inp[0] == "adv" or inp[0] == "adventure":
-            advtype = input("What kind of adventure?")
+            advtype = input("What kind of adventure? > ")
             new = Adventure(advtype)
             print(new.desc())
         elif inp[0] == "q" or inp[0] == "quit":
             break
 
 
-def charLoop():
-
-    print(" - Input 'n' or 'none' to generate character(s). Input 'q' or 'quit' to end program prematurely.\n -")
-    print(" - Available options:\n - setting\n - race\n - gender\n - system\n - char(acter)type\n - prof(ession)\n - namesource\n -")
-
-    # key in main scope so it can be checked in main loop if 'q'/'quit' in subloop
-    key = ''
-    inpDict = {} # hold key/values to change in a dict so they can be passed to Character()
-
-    # Subloop asks for key, then value. Completes on n/none and quits program on q/quit
-    while True:
-        key = input(" - Key to alter: ")
-        if key == 'q' or key == 'quit' or key == 'n' or key == 'none':
-            break # Subloop break condition (on n/none or q/quit)
-        else:
-            value = input(" - Value to set: ")
-            inpDict[key] = value
-            print(" - ")
-
-    # Main loop break condition (on q/quit; continue on n/none)
-    if key == 'q' or key == 'quit':
-        return True
-
-    # How many characters to make?
-    num = int(input(" - Number to generate? "))
-
-    # Make that many and print their names, using data from inp(ut)Dict
-    for i in range(num):
-        temp = Character(**inpDict)
-        print(" -> \n" + temp.desc())
+# def charLoop():
+# 
+#     print(" - Input 'n' or 'none' to generate character(s). Input 'q' or 'quit' to end program prematurely.\n -")
+#     print(" - Available options:\n - setting\n - race\n - gender\n - system\n - char(acter)type\n - prof(ession)\n - namesource\n -")
+# 
+#     # key in main scope so it can be checked in main loop if 'q'/'quit' in subloop
+#     key = ''
+#     inpDict = {} # hold key/values to change in a dict so they can be passed to Character()
+# 
+#     # Subloop asks for key, then value. Completes on n/none and quits program on q/quit
+#     while True:
+#         key = input(" - Key to alter: ")
+#         if key == 'q' or key == 'quit' or key == 'n' or key == 'none':
+#             break # Subloop break condition (on n/none or q/quit)
+#         else:
+#             value = input(" - Value to set: ")
+#             inpDict[key] = value
+#             print(" - ")
+# 
+#     # Main loop break condition (on q/quit; continue on n/none)
+#     if key == 'q' or key == 'quit':
+#         return True
+# 
+#     # How many characters to make?
+#     num = int(input(" - Number to generate? "))
+# 
+#     # Make that many and print their names, using data from inp(ut)Dict
+#     for i in range(num):
+#         temp = Character(**inpDict)
+#         print(" -> \n" + temp.desc())
 
 
 mainLoop()
