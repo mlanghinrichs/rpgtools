5c2e87cee7448b95701dfcd25ecd21ab063ef877
Matt Langhinrichs <mlanghinrichs@gmail.com>
2018-11-25 03:17:34 -0500

    Added DNDChar() subclass in rpg.py

--- debug.py
+++ debug.py
@@ -1,16 +1,16 @@
 import time
 from functools import wraps
 
-# Timer wrapper for debugging hefty probability functions
-def time_me(f):
-    @wraps(f)
+def time_me(func):
+    """Wrap a function to time its execution and print to console."""
+    @wraps(func)
     def dec(*args, **kwargs):
         # Initiate stopwatch (ms)
         curr = int(round(time.time() * 1000))
-
-        r = f(*args, **kwargs)
-
+        # Execute func()
+        r = func(*args, **kwargs)
         # Clock out (ms)
         print("Time elapsed: %d" % (int(round(time.time() * 1000)) - curr))
+        # Return whatever value func() produced
         return r
     return dec
--- rpg.py
+++ rpg.py
@@ -1,30 +1,33 @@
 from random import choice, randint, sample
-import json, os, dnd, gsys
+import json
+import os
+import dnd
+import gsys
 
-# Import json name, adventure data into dicts before doing anything else
+# Import json name, adventure data into dicts
 def load_dict(filename):
     with open("./tbls/" + filename, "r") as f:
         return json.load(f)
-
 char_dict = load_dict("char.json")
 adv_dict = load_dict("adv.json")
 
-def extract(dict_, *args):
-    """Return dict_[arg1][arg2]... if the value is a string or list."""
 
-    current = dict_ ## working dict starts as dict_
-    
+def extract(dict_, *args):
+    """Return dict_[arg 1][arg 2]...[arg n]""" 
+
+    current = dict_ 
     try:
         for arg in args[:-1]:
             current = current[arg]
         return current[args[-1]]
     except KeyError:
         # Return a list with blank str so name generation uses choice() and not ngen_l()
-        print(f"Tried to pull a nonexistent generator {args} with extract! Returning ['']")
+        print(f"Failed to pull {args} with rpg.extract()! Returning ['']")
         return ([''])
 
 
 def extract_choice(dct, *args):
+    """Extract() a list and return a random.choice()"""
     lst = extract(dct, *args)
     try:
         return choice(lst)
@@ -34,21 +37,19 @@ def extract_choice(dct, *args):
 
 
 def ngen_l(list_):
-    """n(ame)gen_l(ist) - Return a name string generated from inputted generator list."""
-    """Input format: 'x,' for choice of x or nul; 'x,y' for choice of x or y; 'x,y,'
-    for a choice of x or y or nul. Pass a list in the order of generation, choices from which 
-    will be concatenated."""
+    """Return a name string generated from inputted generator list."""
+    """Input format: 'x,' for choice of x or nul; 'x,y' for choice of x or y;
+    'x,y,' for a choice of x or y or nul. Pass a list in the order of
+    generation, choices from which will be concatenated."""
     
     out = ""
-
     # For each line of the generator file, append one of the options on that line to out
     for string in list_:
         out += choice(string.split(","))
-    
     # TODO - make this if/else to avoid sneaky bugs?
     # Try to capitalize out properly; if it's < 2 chars, skip this to avoid error 
     try: 
-        return out[0].upper() + out[1:].lower()
+        return out.capitalize() 
     except IndexError:
         return out.upper()
 
@@ -70,46 +71,23 @@ class Character:
     """A class for RPG characters. Can generate PCs or NPCs with names &c."""
 
     # Initialize with any number of customized kwargs including skills, inventory, or stats.
-    def __init__(self, setting="fantasy", race="human", gender="female", *, system="dnd",
-                    chartype="npc", prof="fighter", skills={}, inv={}, stats={}):
+    def __init__(self, setting="fantasy", race="human", gender="female", *,
+                    chartype="npc", prof="fighter", skills={}, inv={}):
 
         # Import each named var above as a local var for the instance
         self.__dict__.update((k, v) for k,v in vars().items() if k != 'self')
-
         # Generate name and surname from value provided
         self.name = ngen_j(setting, race, gender)
         self.surname = ngen_j(setting, race, 'surnames')
-
         # Get agerange list (should be two items) from char_dict - TODO add exception handling
         self.age = randint(*extract(char_dict, setting, race, 'agerange'))
-
         # Get features
         for feature in ('quirk', 'strength', 'flaw', 'desire', 'fear'):
             self.__dict__[feature] = extract_choice(char_dict, setting, feature)
 
-        if system == "dnd":
-            self._rawGen = dnd.Char()
-            self.stats = self._rawGen.stats
-            self.mods = self._rawGen.mods
-
     # TODO comment this formatting mess
     def __str__(self):
-        rstr = "Name: " + self.name + " " + self.surname
-        rstr += "\nAge/Gender: " + str(self.age) + "/" + self.gender
-        rstr += "\nRace: " + self.race
-        for feature in ('quirk', 'strength', 'flaw', 'desire', 'fear'):
-            title = feature[0].upper() + feature[1:]
-            rstr += f"\n{title}: {self.__dict__[feature]}"
-        if len(self.stats) != 0:
-            for (k, v) in self.stats.items():
-                rstr += f"\n{k}: {v}({self.mods[k]})"
-        # Split all output lines into a list so they can be uniformly formatted and then rejoined
-        out = []
-        for line in rstr.split("\n"):
-            h1 = line.split()[0]
-            h2 = " ".join(line.split()[1:])
-            h1 += (12 - len(h1)) * "-"
-            out.append(h1 + h2)
+        out = [(k.ljust(15,"-") + str(v)) for (k,v) in self.__dict__.items()]
         return "\n".join(out)
 
     @classmethod
@@ -120,6 +98,41 @@ class Character:
         race = choice(list(races))
         gender = choice(('male', 'female'))
         return cls(setting, race, gender)
+
+
+class DNDChar(Character):
+
+    def __init__(self, *args, level=0, **kwargs): 
+        self._rawGen = dnd.Char()
+        self.stats = self._rawGen.stats
+        self.mods = self._rawGen.mods
+        self.level = level
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        # Add whitespace before num to make len 3
+        def pad(num, amount=3):
+            return str(num).rjust(amount, " ")
+        def padmod(num, amount=3):
+            """Pad & format modifier"""
+            if num < 0:
+                return pad(num, amount)
+            else:
+                return pad("+" + str(num), amount)
+        out = f"{self.name} {self.surname}"
+        out += (f"\n{self.race.capitalize()} ({self.gender[0]}), {self.age} years old"
+                f"\nLevel {self.level} {self.prof.capitalize()}")
+                # I'm so sorry, PEP 8
+        out += ("\n-------------"
+                "\n|STR DEX CON|"
+                "\n|{0} {1} {2}|".format(*[pad(self.stats[x]) for x in ('str', 'dex', 'con')])
+               +"\n|{0} {1} {2}|".format(*[padmod(self.mods[x]) for x in ('str', 'dex', 'con')])
+               +"\n-------------"
+                "\n|INT WIS CHA|"
+                "\n|{0} {1} {2}|".format(*[pad(self.stats[x]) for x in ('int', 'wis', 'cha')])
+               +"\n|{0} {1} {2}|".format(*[padmod(self.mods[x]) for x in ('int', 'wis', 'cha')])
+               +"\n-------------")
+        return out
 
 
 class Adventure:
@@ -192,9 +205,15 @@ def mainLoop():
                 print("- q(uit): quit program")
             elif inp[0] == "char" or inp[0] == "character":
                 if inp[1] == "random":
-                    char = Character.random('fantasy')
+                    char = Character.random()
                 else:
                     char = Character(*inp[1:]) ## Pass args after 'char'/'character' to Character
+                print(char)
+            elif inp[0] == "dndchar":
+                if inp[1] == "random":
+                    char = DNDChar.random()
+                else:
+                    char = DNDChar(*inp[1:])
                 print(char)
             elif inp[0] == "adv" or inp[0] == "adventure":
                 new = Adventure(inp[1])
@@ -203,7 +222,7 @@ def mainLoop():
                 break
             else:
                 print(f" x {inp[0]} is not a supported command.")
-        except IndexError:
+        except FileNotFoundError:
             print(f"'{' '.join(inp)}' requires a command or more arguments.")
 
 if __name__ == "__main__":
